a555c17a21c4f7e8ddf04a79a952faab63134337
Benjamin Read <endymion1818@gmail.com>
2018-12-26 22:09:42 +0000

    a bit of renaming, adding a little security to the register user form

--- imports/ui/RegisterForm.js
+++ imports/ui/RegisterForm.js
@@ -7,20 +7,23 @@ export default class RegisterForm extends Component {
   }
   registerUser = e => {
     e.preventDefault()
-    Accounts.createUser(
-      {
-        email: this.email.value,
-        password: this.password.value
-      },
-      error => {
-        if (error) {
-          this.setState({
-            error: error.message
-          })
+    if (this.theanswer.value == "fortytwo") {
+      Accounts.createUser(
+        {
+          email: this.email.value,
+          password: this.password.value
+        },
+        error => {
+          if (error) {
+            this.setState({
+              error: error.message
+            })
+          }
+          this.props.client.resetStore()
         }
-        this.props.client.resetStore()
-      }
-    )
+      )
+    }
+    console.log("that's the wrong answer")
   }
 
   render() {
@@ -30,6 +33,12 @@ export default class RegisterForm extends Component {
         <form onSubmit={this.registerUser}>
           <input type="email" ref={input => (this.email = input)} />
           <input type="password" ref={input => (this.password = input)} />
+          <label htmlFor="theanswer">What is the answer?</label>
+          <input
+            type="text"
+            name="theanswer"
+            ref={input => (this.theanswer = input)}
+          />
           <button type="submit">Register User</button>
         </form>
       </div>
